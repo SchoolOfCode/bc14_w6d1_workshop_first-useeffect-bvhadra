81547237db60c93019cc515d9272d9bf7ee8d7b2
bidhan-vhadra <bvhadra@gmail.com>
2023-04-20 15:15:36 +0100

    Task 1 updated

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,9 +9,17 @@ function App() {
   console.log("App rerender");
   const [toDos, setToDos] = useState(["test"]);
 
+ /*
+change the page title to `<NUMBER_OF_THINGS> things to do...` (keep in mind you need to replace `NUMBER_OF_THINGS` with an actual value). This should change every time the length of things to do changes. By interacting with the DOM directly, we are reaching outside of React as a side effect, so we should use the effect hook.
+ */
+
+//----------------------------
+
   useEffect(() => {
     document.title = `${toDos.length} things to do...`;
   }, [toDos]);
+
+//----------------------------
 
   function handleDelete(i) {
     console.log("%chandle delete", "color:lightblue");
